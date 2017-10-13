586bfe9d22b8df5f79dadfc4334bdd2bb1dcc874
perltech <noah.perlmutter@gmail.com>
2017-10-13 11:27:08 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+Pseudo Code
+Construct and import permission schemas
+Then switch depending on 
+*/
+let customer = require('./customer.js')
+
+
+let permissions = process.argv[2];
+let searchArg = process.argv[3]
+
+switch(permissions) {
+	case 'customer':
+		
+		break;
+}
